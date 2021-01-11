fbb9e1052f39722f2b7c9b3a602335ff0df0c466
male524 <male524@users.noreply.github.com>
2021-01-11 14:57:25 -0800

    yay

---
+++ numJewelsInStones.py
@@ -0,0 +1,39 @@
+# 771. Jewels and Stones
+"""
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+ 
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+ 
+
+Constraints:
+
+1 <= jewels.length, stones.length <= 50
+jewels and stones consist of only English letters.
+All the characters of jewels are unique.
+"""
+
+class NumJewelsInStones(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        numJewels = 0
+        for i in jewels:
+            countJewels = stones.count(i)
+            numJewels += countJewels
+        return numJewels
+
+# I learned that saying "for i in string" sets i equal to the value of the index of the string you set it to.
