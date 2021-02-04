1b17c34c7efeec7b09743c7f98e0e68864fa1de2
male524 <male524@users.noreply.github.com>
2021-02-04 15:32:22 -0800

    yay

--- Week 4/toLowerCase.py
+++ Week 4/toLowerCase.py
@@ -22,3 +22,5 @@ class Solution:
     def toLowerCase(self, str: str) -> str:
         return ''.join([chr(ord(char) + 32) if 65 <= ord(char) <= 90 else char for char in str])
 
+# I learned how to use list comprehension a bit better from w3schools.
+# This took me 30 minutes since I challenged my self to solve this in one line.
