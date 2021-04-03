3405379cfda4577ae19bb4779edd880059f2a87a
male524 <maleygray524@gmail.com>
2021-04-03 16:29:48 -0700

    yay

---
+++ Week 9/sortArray.py
@@ -0,0 +1,61 @@
+# 912. Sort an Array
+"""
+iven an array of integers nums, sort the array in ascending order.
+
+ 
+
+Example 1:
+
+Input: nums = [5,2,3,1]
+Output: [1,2,3,5]
+Example 2:
+
+Input: nums = [5,1,1,2,0,0]
+Output: [0,0,1,1,2,5]
+ 
+
+Constraints:
+
+1 <= nums.length <= 50000
+-50000 <= nums[i] <= 50000
+"""
+
+from typing import List
+
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        self.mergeSort(nums)
+        return nums
+    def mergeSort(self, arr):
+        if len(arr) > 1:
+            mid = len(arr) // 2
+            L = arr[:mid]
+            R = arr[mid:]
+            self.mergeSort(L)
+            self.mergeSort(R)
+            i = j = k = 0
+            while i < len(L) and j < len(R):
+                if L[i] < R[j]:
+                    arr[k] = L[i]
+                    i += 1
+                else:
+                    arr[k] = R[j]
+                    j += 1
+                k += 1
+            
+            while i < len(L):
+                arr[k] = L[i]
+                i += 1
+                k += 1
+            
+            while j < len(R):
+                arr[k] = R[j]
+                j += 1
+                k += 1
+
+print(Solution.sortArray(1, [5,1,1,2,0,0]))
+
+# I used the built in sorting function to solve this problem
+# This took like 1 minute since I kinda cheated
+# Write a quick sort and a merge sort
